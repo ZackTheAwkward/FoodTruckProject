6ed97dc0422b2d5eb2462f74fdf99d68186da5a8
Zack Gaddy <zack.e.gaddy@gmail.com>
2022-02-25 19:12:29 -0700

    laid down a basic outline to turn into UML

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -1,5 +1,9 @@
 package com.skilldistillery.foodtruck;
 
 public class FoodTruck {
+//TODO define fields for a unique numeric id, a name (Name of FoodTruck), and a numeric rating.
+	//Refer to Cars/ParkingLot drills in Encapsulation drills
+	
 
+ 
 }
--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -3,8 +3,47 @@ package com.skilldistillery.foodtruck;
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Create an array to store up to 5 food truck objects
+		// THIS MAIN WILL BE THE ONLY STATIC METHOD
 
+		/*
+		 * TODO USER STORY #1 The user is prompted to input the name, food type, and
+		 * rating for up to five food trucks. For each set of input, a FoodTruck object
+		 * is created, its fields set to the user's input, and it is added to the array.
+		 * The truck id is not input by the user, but instead assigned automatically in
+		 * the FoodTruck constructor from a static field that is incremented as each
+		 * truck is created.
+		 */
+		// Refer to Car and ParkingLot drills in Encapsulation Drills
+
+		/*
+		 * TODO USER STORY 2 If the user inputs quit for the food truck name, input ends
+		 * immediately and the program continues.
+		 */
+		// boolean keepGoing (isTrue) will probably help with the quit option as well
+		// .toUpperCase and .equals
+
+		/*
+		 * TODO USER STORY 3 After input is complete, the user sees a menu from which
+		 * they can choose to:
+		 * 
+		 * List all existing food trucks. 
+		 * See the average rating of food trucks. 
+		 * Display the highest-rated food truck. 
+		 * Quit the program.
+		 * 
+		 */
+		//Look into using a switch to print a menu
+		
+		
+		/*TODO
+		 * USER STORY 4
+		 * After choosing a menu item, the user sees the menu again and can choose another item until the choose to quit		 
+		 */
+		// Using either a for loop or while loop to keep the menu going until quit is chosen. (.toUpperCasse and .equals)
+		
+		
+		
 	}
 
 }
